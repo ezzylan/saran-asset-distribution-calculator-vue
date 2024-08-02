621f66c9ab0e0091dbc5e57cd32e8bcf30691489
Ezlan Zulfiqree <66157842+ezzylan@users.noreply.github.com>
2024-08-02 16:48:51 +0800

    feat: add referral input field

--- src/components/CalcForm.vue
+++ src/components/CalcForm.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { startCase } from "lodash-es";
 import { Baby, HandHeart, Users } from "lucide-vue-next";
 import isMobilePhone from "validator/lib/isMobilePhone";
 import { useForm } from "vee-validate";
@@ -16,6 +17,7 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { toTypedSchema } from "@vee-validate/zod";
 import { useFetch } from "@vueuse/core";
 
@@ -30,34 +32,33 @@ const emit = defineEmits<{
   ): void;
 }>();
 
-function calculatePercentage(results: { items: string[] }) {
+function calculatePercentage(items: string[]) {
   let parentsPerc = 0;
   let spousePerc = 0;
   let childrenPerc = 0;
 
-  if (results.items.includes("parentsPresent")) {
+  if (items.includes("parentsPresent")) {
     parentsPerc =
-      !results.items.includes("spousePresent") &&
-      !results.items.includes("childrenPresent")
+      !items.includes("spousePresent") && !items.includes("childrenPresent")
         ? 100
         : 50;
 
-    if (results.items.includes("spousePresent")) {
-      spousePerc = results.items.includes("childrenPresent") ? 25 : 50;
-      childrenPerc = results.items.includes("childrenPresent") ? 25 : 0;
+    if (items.includes("spousePresent")) {
+      spousePerc = items.includes("childrenPresent") ? 25 : 50;
+      childrenPerc = items.includes("childrenPresent") ? 25 : 0;
     } else {
       spousePerc = 0;
-      childrenPerc = results.items.includes("childrenPresent") ? 50 : 0;
+      childrenPerc = items.includes("childrenPresent") ? 50 : 0;
     }
   } else {
     parentsPerc = 0;
 
-    if (results.items.includes("spousePresent")) {
-      spousePerc = results.items.includes("childrenPresent") ? 50 : 100;
-      childrenPerc = results.items.includes("childrenPresent") ? 50 : 0;
+    if (items.includes("spousePresent")) {
+      spousePerc = items.includes("childrenPresent") ? 50 : 100;
+      childrenPerc = items.includes("childrenPresent") ? 50 : 0;
     } else {
       spousePerc = 0;
-      childrenPerc = results.items.includes("childrenPresent") ? 100 : 0;
+      childrenPerc = items.includes("childrenPresent") ? 100 : 0;
     }
   }
 
@@ -81,22 +82,34 @@ const formSchema = toTypedSchema(
     }),
     childrenNumber: z.coerce.number().int().gte(1).lte(10).optional(),
     mobileNumber: z.string().min(9).max(10).refine(isMobilePhone),
+    referral: z.enum(["TikTok", "Instagram", "Facebook", "Person"], {
+      required_error: "You need to select a referral.",
+    }),
+    referralPerson: z
+      .string()
+      .min(3, {
+        message: "Name must be at least 3 characters.",
+      })
+      .optional(),
   }),
 );
 
-const { handleSubmit } = useForm({
+const { values, handleSubmit } = useForm({
   validationSchema: formSchema,
   initialValues: {
     items: ["parentsPresent"],
+    referral: "TikTok",
   },
 });
 
 const onSubmit = handleSubmit((values) => {
-  const { parentsPerc, spousePerc, childrenPerc } = calculatePercentage(values);
+  const { parentsPerc, spousePerc, childrenPerc } = calculatePercentage(
+    values.items,
+  );
 
   let bodyText = `
     New client!
-    Name: ${values.name}
+    Name: ${startCase(values.name)}
     Email: ${values.email}
     Mobile Number: +60${values.mobileNumber}
     Parents: ${parentsPerc}%
@@ -105,14 +118,16 @@ const onSubmit = handleSubmit((values) => {
     `;
 
   if (values.items.includes("childrenPresent")) {
-    bodyText += `Number of Children: ${values.childrenNumber}`;
+    bodyText += `Number of Children: ${values.childrenNumber}\n\t`;
   }
 
-  let telegramBotUrl = `https://api.telegram.org/bot${import.meta.env.VITE_TELEGRAM_BOT_TOKEN}/sendMessage`;
+  bodyText += `Referral: ${values.referral === "Person" ? startCase(values.referralPerson) : values.referral}`;
 
   let bodyContent = new FormData();
   bodyContent.append("chat_id", "@tsassetdistcalc");
   bodyContent.append("text", bodyText);
+
+  let telegramBotUrl = `https://api.telegram.org/bot${import.meta.env.VITE_TELEGRAM_BOT_TOKEN}/sendMessage`;
 
   const { error } = useFetch(telegramBotUrl).post(bodyContent);
   if (error.value) {
@@ -125,98 +140,162 @@ const onSubmit = handleSubmit((values) => {
 
 <template>
   <form class="space-y-6 pt-6" @submit="onSubmit">
-    <FormField v-slot="{ componentField }" name="name">
-      <FormItem>
-        <FormLabel>Name</FormLabel>
-        <FormControl>
-          <Input required v-bind="componentField" />
-        </FormControl>
-        <FormMessage />
-      </FormItem>
-    </FormField>
+    <div class="flex flex-col gap-6 sm:flex-row">
+      <FormField v-slot="{ componentField }" name="name">
+        <FormItem class="grow">
+          <FormLabel>Name</FormLabel>
+          <FormControl>
+            <Input required v-bind="componentField" />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      </FormField>
+      <FormField v-slot="{ componentField }" name="email">
+        <FormItem class="grow">
+          <FormLabel>Email</FormLabel>
+          <FormControl>
+            <Input required v-bind="componentField" />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      </FormField>
 
-    <FormField v-slot="{ componentField }" name="email">
-      <FormItem>
-        <FormLabel>Email</FormLabel>
-        <FormControl>
-          <Input required v-bind="componentField" />
-        </FormControl>
-        <FormMessage />
-      </FormItem>
-    </FormField>
-
-    <FormField name="items">
-      <FormItem>
-        <div class="flex flex-col gap-2 md:flex-row md:gap-16">
-          <FormField
-            v-for="item in items"
-            v-slot="{ value, handleChange }"
-            :key="item.id"
-            type="checkbox"
-            :value="item.id"
-            :unchecked-value="false"
-            name="items"
-          >
-            <div class="space-y-3">
-              <FormItem class="flex flex-row items-start space-x-3 space-y-0">
-                <FormControl>
-                  <Checkbox
-                    :checked="value.includes(item.id)"
-                    @update:checked="handleChange"
-                  />
-                </FormControl>
-                <FormLabel class="font-normal">
-                  {{ item.label }}
-                </FormLabel>
-                <Users v-if="item.id === 'parentsPresent'" class="h-4 w-4" />
-                <HandHeart v-if="item.id === 'spousePresent'" class="h-4 w-4" />
-                <Baby v-if="item.id === 'childrenPresent'" class="h-4 w-4" />
-              </FormItem>
-              <FormField
-                v-if="
-                  item.id === 'childrenPresent' &&
-                  value.includes('childrenPresent')
-                "
-                v-slot="{ componentField }"
-                name="childrenNumber"
-              >
-                <FormItem>
+      <FormField name="items">
+        <FormItem class="basis-1/4">
+          <div class="flex flex-col gap-3">
+            <FormField
+              v-for="item in items"
+              v-slot="{ value, handleChange }"
+              :key="item.id"
+              type="checkbox"
+              :value="item.id"
+              :unchecked-value="false"
+              name="items"
+            >
+              <div class="space-y-3">
+                <FormItem class="flex flex-row items-start space-x-3 space-y-0">
                   <FormControl>
-                    <Input
-                      type="number"
-                      min="1"
-                      max="10"
-                      placeholder="How many?"
-                      v-bind="componentField"
-                      class="w-32"
+                    <Checkbox
+                      :checked="value.includes(item.id)"
+                      @update:checked="handleChange"
                     />
                   </FormControl>
-                  <FormMessage />
+                  <FormLabel class="font-normal">
+                    {{ item.label }}
+                  </FormLabel>
+                  <Users v-if="item.id === 'parentsPresent'" class="h-4 w-4" />
+                  <HandHeart
+                    v-if="item.id === 'spousePresent'"
+                    class="h-4 w-4"
+                  />
+                  <Baby v-if="item.id === 'childrenPresent'" class="h-4 w-4" />
                 </FormItem>
-              </FormField>
-            </div>
-          </FormField>
-        </div>
-        <FormMessage />
-      </FormItem>
-    </FormField>
 
-    <FormField v-slot="{ componentField }" name="mobileNumber">
-      <FormItem>
-        <FormLabel>Mobile Number</FormLabel>
-        <div class="flex">
-          <Button class="bg-foreground" disabled>+60</Button>
-          <FormControl>
-            <Input type="tel" required v-bind="componentField" />
-          </FormControl>
-        </div>
-        <FormDescription>
-          Provide your WhatsApp number to receive free illustrations of your
-          Asset Distribution under the 1958 Malaysian Distribution Act.
-        </FormDescription>
-        <FormMessage />
-      </FormItem>
-    </FormField>
+                <FormField
+                  v-if="
+                    item.id === 'childrenPresent' &&
+                    value.includes('childrenPresent')
+                  "
+                  v-slot="{ componentField }"
+                  name="childrenNumber"
+                >
+                  <FormItem>
+                    <FormControl>
+                      <Input
+                        type="number"
+                        min="1"
+                        max="10"
+                        placeholder="How many?"
+                        v-bind="componentField"
+                        class="w-32"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                </FormField>
+              </div>
+            </FormField>
+          </div>
+          <FormMessage />
+        </FormItem>
+      </FormField>
+    </div>
+
+    <div class="flex flex-col gap-6 sm:flex-row">
+      <FormField v-slot="{ componentField }" name="mobileNumber">
+        <FormItem class="basis-3/4">
+          <FormLabel>Mobile Number</FormLabel>
+          <div class="flex">
+            <Button class="bg-foreground" disabled>+60</Button>
+            <FormControl>
+              <Input type="tel" required v-bind="componentField" />
+            </FormControl>
+          </div>
+          <FormDescription>
+            Provide your WhatsApp number to receive free illustrations of your
+            Asset Distribution under the 1958 Malaysian Distribution Act.
+          </FormDescription>
+          <FormMessage />
+        </FormItem>
+      </FormField>
+
+      <div class="space-y-3">
+        <FormField v-slot="{ componentField }" type="radio" name="referral">
+          <FormItem class="space-y-3">
+            <FormLabel>Referral</FormLabel>
+            <FormControl>
+              <RadioGroup
+                class="flex flex-col space-y-1"
+                v-bind="componentField"
+              >
+                <FormItem class="flex items-center gap-x-3 space-y-0">
+                  <FormControl>
+                    <RadioGroupItem value="TikTok" />
+                  </FormControl>
+                  <FormLabel class="font-normal"> TikTok </FormLabel>
+                </FormItem>
+                <FormItem class="flex items-center gap-x-3 space-y-0">
+                  <FormControl>
+                    <RadioGroupItem value="Instagram" />
+                  </FormControl>
+                  <FormLabel class="font-normal"> Instagram </FormLabel>
+                </FormItem>
+                <FormItem class="flex items-center gap-x-3 space-y-0">
+                  <FormControl>
+                    <RadioGroupItem value="Facebook" />
+                  </FormControl>
+                  <FormLabel class="font-normal"> Facebook </FormLabel>
+                </FormItem>
+                <FormItem class="flex items-center gap-x-3 space-y-0">
+                  <FormControl>
+                    <RadioGroupItem value="Person" />
+                  </FormControl>
+                  <FormLabel class="font-normal"> Person </FormLabel>
+                </FormItem>
+              </RadioGroup>
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        </FormField>
+
+        <FormField
+          v-if="values.referral === 'Person'"
+          v-slot="{ componentField }"
+          name="referralPerson"
+        >
+          <FormItem>
+            <FormControl>
+              <Input
+                type="text"
+                placeholder="Referral Name"
+                v-bind="componentField"
+              />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        </FormField>
+      </div>
+    </div>
 
     <Button type="submit">Submit</Button>
   </form>
